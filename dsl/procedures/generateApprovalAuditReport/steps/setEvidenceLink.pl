df01a545f9d3f41acbb2829dbcc20e8e19e3ed41
Kenneth McKnight <kmcknight@electric-cloud.com>
2019-02-15 14:56:35 -0800

    Fixes #12 - added target=_blank so reports open in their own tab

--- dsl/procedures/generateApprovalAuditReport/steps/setEvidenceLink.pl
+++ dsl/procedures/generateApprovalAuditReport/steps/setEvidenceLink.pl
@@ -11,4 +11,4 @@ $[/myProject/scripts/perlHeaderJSON]
 
 # set pipeline stage summary link to access unplug generated report
 $ec->setProperty("/myPipelineStageRuntime/ec_summary/Approval Audit:", 
-   '<html><a href="/commander/pages/unplug/un_run1?flowRuntimeId=$[/myPipelineRuntime/flowRuntimeId]">Summary Report</a></html>');
+   '<html><a target="_blank" href="/commander/pages/unplug/un_run1?flowRuntimeId=$[/myPipelineRuntime/flowRuntimeId]">Summary Report</a></html>');
--- dsl/procedures/generateProductionAuditReport/steps/setEvidenceLink.pl
+++ dsl/procedures/generateProductionAuditReport/steps/setEvidenceLink.pl
@@ -11,4 +11,4 @@ $[/myProject/scripts/perlHeaderJSON]
 
 # set pipeline stage summary link to access unplug generated report
 $ec->setProperty("/myPipelineStageRuntime/ec_summary/Production Audit:", 
-   '<html><a href="/commander/pages/unplug/un_run9?flowRuntimeId=$[/myPipelineRuntime/flowRuntimeId]">Summary Report</a></html>');
+   '<html><a target="_blank" href="/commander/pages/unplug/un_run9?flowRuntimeId=$[/myPipelineRuntime/flowRuntimeId]">Summary Report</a></html>');
