83fdedab09000f6af05e633d4ccf58aaaba2b7cd
Greg Maxey <gregorymaxey@yahoo.com>
2020-01-16 23:57:25 +0000

    Add manual tasks to approval report, closes #2

--- ecpluginbuilder.pl
+++ ecpluginbuilder.pl
@@ -14,7 +14,7 @@ my $ec = new ElectricCommander->new({timeout => 420});
 
 my $epb="../ecpluginbuilder";
 
-my $pluginVersion = "1.3.0";
+my $pluginVersion = "1.4.0";
 my $pluginKey = "EC-AuditReports";
 
 GetOptions ("version=s" => \$pluginVersion)
