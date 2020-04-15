4a0be95aa783f77565bafdd9d13535b9c0cd3f88
Polina Shubina <925043@gmail.com>
2020-04-15 17:07:18 +0300

    Fixed $ expansion in the setup script

--- ec_setup_custom.pl
+++ ec_setup_custom.pl
@@ -1,13 +1,10 @@
-# ec_setup_custom.pl
-
-
 if ($promoteAction eq 'promote') {
-	$commander->setProperty("/server/unplug/va", {value=>'$[/plugins/EC-AuditReports/project/audit_reports/approvalAudit]'});
-	$commander->setProperty("/server/unplug/vb", {value=>'$[/plugins/EC-AuditReports/project/audit_reports/timingAudit]'});
-	$commander->setProperty("/server/unplug/vc", {value=>'$[/plugins/EC-AuditReports/project/audit_reports/evidenceAudit]'});
+    $commander->setProperty("/server/unplug/va", {value=>'$' . '[/plugins/EC-AuditReports/project/audit_reports/approvalAudit]'});
+    $commander->setProperty("/server/unplug/vb", {value=>'$' . '[/plugins/EC-AuditReports/project/audit_reports/timingAudit]'});
+    $commander->setProperty("/server/unplug/vc", {value=>'$' . '[/plugins/EC-AuditReports/project/audit_reports/evidenceAudit]'});
 }
 else {
-	foreach ("va", "vb", "vc") {
-		$commander->setProperty("/server/unplug/$_", {value=>"\$[/plugins/unplug/project/v_example$_]"});
-	}
+    foreach ("va", "vb", "vc") {
+        $commander->setProperty("/server/unplug/$_", {value=>"\$" . "[/plugins/unplug/project/v_example$_]"});
+    }
 }
